dbb2c77f3b5df29c63c9d32e86fecfa11fc0d5bc
Zubair Nurie <mznurie@msn.com>
2020-02-01 01:25:04 -0500

    Added a default home page.

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'makeposts.apps.MakepostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
