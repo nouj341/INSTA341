42a517e2f9d0f070632bf56eef0d9ae335860b19
Zubair Nurie <mznurie@msn.com>
2020-02-04 18:35:00 -0500

    Issue #21 resolved: added backend functionality for user login and logout

--- app/users/views.py
+++ app/users/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Account created successfully for {username}!')
             return redirect('insta-home')
     else:
         form = UserRegisterForm()
