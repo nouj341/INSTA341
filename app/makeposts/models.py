e805f7347a72541f88bbcabcbfa7d5f8bf7c32fa
Amneet  Singh <amneet.s.270@gmail.com>
2020-03-04 18:17:12 -0500

    #45 Readd elements to .html files after merge

--- app/makeposts/models.py
+++ app/makeposts/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        return reverse('post_detail', kwargs={'pk': self.pk})
 
 
 class Following(models.Model):
