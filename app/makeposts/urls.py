c4a980b44e0eb7a07641550396cb5be54dc85cd6
Zubair Nurie <mznurie@msn.com>
2020-03-07 22:14:37 -0500

    #44, #46 Add profile page view for all users. Follow feature changed along with the urls

--- app/makeposts/urls.py
+++ app/makeposts/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='insta-home'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/<int:pk>/follow', views.follow, name='follow'),
-    path('post/<int:pk>/unfollow', views.unfollow, name='unfollow'),
+    path('profile/<str:username>/follow', views.follow, name='follow'),
+    path('profile/<str:username>/unfollow', views.unfollow, name='unfollow'),
     path('post/<int:pk>/comment', views.comment, name="comment"),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('profile/<str:username>/', views.user_profile_view, name='profile-view')
 
 ]
--- app/makeposts/views.py
+++ app/makeposts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse
 from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post, Following, Comments
 from .forms import CommentForm
 
@@ -14,21 +15,22 @@ def home(request):
     return render(request, 'makeposts/home.html', context)
 
 
-def follow(request, pk):
+def follow(request, username):
     if request.user and request.user.is_authenticated:
         user = User.objects.get(username=request.user)
-        post_user_id = Post.objects.get(id=pk).author_id
-        following = Following(user_id=post_user_id, follower=user.id)
+        followed_user_id = User.objects.get(username=username).id
+        following = Following(user_id=followed_user_id, follower=user.id)
         following.save()
-        return redirect('/')
+        return redirect(reverse('profile-view', kwargs={'username':username}))
 
-def unfollow(request, pk):
+
+def unfollow(request, username):
     if request.user and request.user.is_authenticated:
         user = User.objects.get(username=request.user)
-        post_user_id = Post.objects.get(id=pk).author_id
-        following = Following.objects.get(user_id=post_user_id, follower=user.id)
+        followed_user_id = User.objects.get(username=username).id
+        following = Following.objects.get(user_id=followed_user_id, follower=user.id)
         following.delete()
-        return redirect('/')
+        return redirect(reverse('profile-view', kwargs={'username':username}))
 
 
 def home(request):
@@ -36,7 +38,7 @@ def home(request):
         user = User.objects.get(username=request.user)
         posts = Post.objects.order_by('-date_posted')
         try:
-            following = [f.user_id  for f in Following.objects.filter(follower=user.id)]
+            following = [f.user_id for f in Following.objects.filter(follower=user.id)]
         except Following.DoesNotExist:
             following = []
         return render(request, 'makeposts/home.html', {
@@ -52,17 +54,28 @@ def comment(request, pk):
             user = User.objects.get(username=request.user)
             form = CommentForm(request.POST)
             if form.is_valid():
-                comment = Comments(post_id=pk, commenter=user.id, comment=form.cleaned_data["comment"], username=request.user)
+                comment = Comments(post_id=pk, commenter=user.id, comment=form.cleaned_data["comment"],
+                                   username=request.user)
                 comment.save()
                 return redirect("/post/" + str(pk))
             else:
                 return HttpResponse("Invalid Form")
 
 
+def user_profile_view(request, username):
+    if request.user and request.user.is_authenticated:
+        user = User.objects.get(username=username)
+        posts = Post.objects.filter(author=user)
+        following = [f.user_id for f in Following.objects.filter(follower=request.user.id)]
+        return render(request, 'makeposts/profile_view.html', {"posts": posts, 'user': user, 'follow':following})
+    else:
+        return redirect("/")
+
+
 def post_detail(request, pk):
     if request.user and request.user.is_authenticated:
         user = User.objects.get(username=request.user)
-        following = [f.user_id  for f in Following.objects.filter(follower=user.id)]
+        following = [f.user_id for f in Following.objects.filter(follower=user.id)]
         post = Post.objects.get(id=pk)
         comments = Comments.objects.filter(post_id=pk).order_by("-comment_time")
         return render(request, "makeposts/post_detail.html", {
@@ -79,13 +92,9 @@ class PostListView(ListView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
+
     fields = ['title', 'content', 'image']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-
-
-
-
-
